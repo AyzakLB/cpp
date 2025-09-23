1095eaedc139a0d899e487a29adeff33b7cb9416
ayzaq <ayoubmarzaq1@gmail.com>
2025-09-23 18:27:20 +0100

    cpp06, ready to push

--- cpp-06/ex02/main.cpp
+++ cpp-06/ex02/main.cpp
@@ -2,6 +2,7 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <cstdlib>
+
 Base * generate(void)
 {
     int randomNumber = rand() % 3;
@@ -16,12 +17,18 @@ Base * generate(void)
 }
 
 void identify(Base* p) {
+    if (!p) {
+        std::cerr << "Null pointer" << std::endl;
+        return;
+    }
     if (dynamic_cast<A*>(p)) {
         std::cout << "A" << std::endl;
     } else if (dynamic_cast<B*>(p)) {
         std::cout << "B" << std::endl;
     } else if (dynamic_cast<C*>(p)) {
         std::cout << "C" << std::endl;
+    } else {
+        std::cout << "Unknown type" << std::endl;
     }
 }
 
@@ -33,33 +40,42 @@ void identify(Base& p)
         std::cout << "A" << std::endl;
     } catch (const std::exception &e)
     {
-        (void)e;
+            try {
+            B &bRef = dynamic_cast<B&>(p);  
+            (void) bRef;
+            std::cout << "B" << std::endl;
+        } catch (const std::exception &e)
+        {
+                try {
+                  C &cRef = dynamic_cast<C&>(p);
+                (void) cRef;
+                std::cout << "C" << std::endl;
+                } catch (const std::exception &e)
+                {
+                    std::cout << e.what() << std::endl;
+                }
+        }
     }
-    try {
-        B &bRef = dynamic_cast<B&>(p);
-        (void) bRef;
-        std::cout << "B" << std::endl;
-    } catch (const std::exception &e)
-    {
-        (void)e;
-    }
-    try {
-        C &cRef = dynamic_cast<C&>(p);
-        (void) cRef;
-        std::cout << "C" << std::endl;
-    } catch (const std::exception &e)
-    {
-        (void)e;
-    }
+    
+    
 }
 
 int main()
 {
-    std::srand(time(0));
-	Base *ptr = generate();
-	Base &ref = *ptr;
-	identify(ptr);
-	identify(ref);
-	delete ptr;
+    Base *a = new A;
+    Base *b = new B;
+    Base *c = new C;
+    Base &aRef = *a;
+    Base &bRef = *b;
+    Base &cRef = *c;
+    identify(a);
+    identify(b);
+    identify(c);
+    identify(aRef);
+    identify(bRef);
+    identify(cRef);
+    delete a;
+    delete b;
+    delete c;
 	return 0;
 }
