b1a0a14bb082f0fd36a14fdf0397b446045f99ed
ayzaq <ayoubmarzaq1@gmail.com>
2025-09-28 12:30:17 +0100

    cpp07 done

--- cpp-07/ex01/main.cpp
+++ cpp-07/ex01/main.cpp
@@ -3,16 +3,17 @@
 template <typename Type1>
 void isTrue(Type1 arg)
 {
-    if (arg)
-        std::cout << "true";
-    else
-        std::cout << "false";
-    std::cout << std::endl;
+    std::cout << arg << std::endl;
 }
 
 int main()
 {
     int arr[] = {1, 2, 3, 4, 0} ;
-    iter(arr, 5, isTrue<int>); 
+
+    std::cout << "int array" << std::endl;
+    iter(arr, 5, isTrue<int>);
+    std::cout << "char array" << std::endl;
+    char str[] = "hello world!";
+    iter(str, 13, isTrue<char>);
     return 0;
 }
