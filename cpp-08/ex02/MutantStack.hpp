6211f296eb0014de6891fbba809e8370dede6d6f
ayzaq <ayoubmarzaq1@gmail.com>
2025-09-30 17:35:55 +0100

    up

--- cpp-08/ex02/MutantStack.hpp
+++ cpp-08/ex02/MutantStack.hpp
@@ -6,6 +6,14 @@ template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
     public:
+    MutantStack() : std::stack<T, Container>(){}
+    MutantStack(const MutantStack &other) : std::stack<T, Container>(other) {}
+    MutantStack &operator=(const MutantStack &other) {
+        if (this != &other)
+            std::stack<T, Container>::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
     typedef typename Container::iterator iterator;
     typedef typename Container::const_iterator const_iterator;
 
